d8efa22646b9828f86a5ab5849b5ad1962411734
willibrown12 <viliking123@gmail.com>
2025-04-20 00:51:05 +0300

    sa

--- AdvancedNodeStarter/test/header.test.js
+++ AdvancedNodeStarter/test/header.test.js
@@ -1,8 +1,35 @@
 const puppeteer = require("puppeteer");
+let browser, page
 
-test("we can laounch a browser ", async () => {
-  const browser = await puppeteer.launch({
-    headless:false
+beforeEach(async () => {
+
+   browser = await puppeteer.launch({
+    headless: false,
   });
-  const page = await browser.newPage();
+   page = await browser.newPage();
+  await page.goto("localhost:3000");
 });
+
+
+afterEach(async () => {
+
+  await browser.close()
+});
+
+test("header has the correct text ", async () => {
+
+  const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+
+  expect(text).toEqual("Blogster");
+});
+
+test("clicking start the oAuth flow", async () => {
+
+  const text = await page.click(".right a");
+
+  const url = await page.url()
+
+  expect(url).toMatch(/accounts\.google\.com/);
+ 
+});
+
